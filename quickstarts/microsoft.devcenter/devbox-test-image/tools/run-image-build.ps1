ad3b3f266ba554dc57306aa699a5de013cda4a67
Dmitry Goncharenko <dgonch@microsoft.com>
2024-06-08 19:42:12 -0700

    more logging in run-image-build

--- quickstarts/microsoft.devcenter/devbox-test-image/tools/run-image-build.ps1
+++ quickstarts/microsoft.devcenter/devbox-test-image/tools/run-image-build.ps1
@@ -3,12 +3,11 @@ Set-StrictMode -Version Latest
 $ProgressPreference = 'SilentlyContinue'
 
 Connect-AzAccount -Identity
+Install-Module -Name Az.ImageBuilder -AllowPrerelease -Force
 # 'Az.ImageBuilder', 'Az.ManagedServiceIdentity' | ForEach-Object { Install-Module -Name $_ -AllowPrerelease -Force }
 
 Write-Host "=== Starting the image build"
 Invoke-AzResourceAction -ResourceName "${env:imageTemplateName}" -ResourceGroupName "${env:resourceGroupName}" -ResourceType "Microsoft.VirtualMachineImages/imageTemplates" -ApiVersion "2020-02-14" -Action Run -Force
-
-Install-Module -Name Az.ImageBuilder -AllowPrerelease -Force
 
 try {
     Write-Host "=== Waiting for the image build to complete"
