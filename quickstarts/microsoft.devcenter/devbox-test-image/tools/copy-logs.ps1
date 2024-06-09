c0d5a34d27baa80bc8d1f9941cad143a653cfd36
Dmitry Goncharenko <dgonch@microsoft.com>
2024-06-09 16:54:10 -0700

    customization.log

--- quickstarts/microsoft.devcenter/devbox-test-image/tools/copy-logs.ps1
+++ quickstarts/microsoft.devcenter/devbox-test-image/tools/copy-logs.ps1
@@ -4,23 +4,24 @@ $ProgressPreference = 'SilentlyContinue'
 
 Connect-AzAccount -Identity
 
-Write-Host "=== Looking for storage account containing customizations log. Staging RG: ${env:imageBuildStagingResourceGroupName}"
+$logsFile = 'customization.log'
+Write-Host "=== Looking for storage account in staging RG: ${env:imageBuildStagingResourceGroupName}"
 $stagingStorageAccountName = (Get-AzResource -ResourceGroupName ${env:imageBuildStagingResourceGroupName} -ResourceType "Microsoft.Storage/storageAccounts")[0].Name
 
 $ctx = New-AzStorageContext -StorageAccountName $stagingStorageAccountName
-$logsBlob = Get-AzStorageBlob -Context $ctx -Container packerlogs | Where-Object { $_.Name -eq 'customization.log' }
+$logsBlob = Get-AzStorageBlob -Context $ctx -Container packerlogs | Where-Object { $_.Name -eq $logsFile }
 if (-not $logsBlob) {
     Write-Host "Could not find customization.log in storage account: $stagingStorageAccountName"
     return
 }
 
-Write-Host "=== Downloading customizations.log in storage account: $stagingStorageAccountName"
-Get-AzStorageBlobContent -Context $ctx -CloudBlob $logsBlob.ICloudBlob -Destination 'customization.log'
+Write-Host "=== Downloading $logsFile in storage account: $stagingStorageAccountName"
+Get-AzStorageBlobContent -Context $ctx -CloudBlob $logsBlob.ICloudBlob -Destination $logsFile
 
-Write-Host "=== Uploading customizations.log to storage account: ${env:logsStorageAccountName}"
+Write-Host "=== Uploading $logsFile to storage account: ${env:logsStorageAccountName}"
 $ctx = New-AzStorageContext -StorageAccountName "${env:logsStorageAccountName}"
 New-AzStorageContainer -Context $ctx -Name logs -Permission 'Blob'
-Set-AzStorageBlobContent -Context $ctx -Container logs -Blob 'customization.log' -StandardBlobTier 'Hot' -File 'customization.log'
+Set-AzStorageBlobContent -Context $ctx -Container logs -Blob $logsFile -StandardBlobTier 'Hot' -File $logsFile
 
 Write-Host "=== Waiting to allow for the logs to be accessed for troubleshooting"
 Start-Sleep -Seconds (15 * 60)  # Wait 15 minutes to allow for the logs to be downloaded
